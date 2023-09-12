dc6f367f8bb591fb01d018c9932a1059e998da35
MartinBINARD <martin.binard.dev@gmail.com>
2023-09-12 14:29:44 +0200

    Routing : route paramétrée

---
+++ src/app/pokemon/bulbizarre/page.tsx
@@ -0,0 +1,7 @@
+export default function Single() {
+  return (
+    <main className="bg-cyan-950 min-h-screen">
+      <h1 className="font-bold text-cyan-400 text-4xl p-12">Fiche Pokémon</h1>
+    </main>
+  );
+}
